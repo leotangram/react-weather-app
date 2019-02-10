743d44165a51ecd04029ecf1a67ce68ae3b2d8db
Leonardo Omana <leotangram@gmail.com>
2019-02-09 21:22:33 -0500

    Transform Data

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 import Location from "./Location";
 import WeatherData from "./WeatherData";
 import "./styles.css";
-import { SUN, WINDY } from "./../../contants/weathers";
+import { SUN } from "./../../contants/weathers";
+
+const location = "Bogota,co";
+const api_key = "76ff146f7db30c279ba914a50a7ab1b8";
+const url_base_weather = "http://api.openweathermap.org/data/2.5/weather/";
+//api.openweathermap.org/data/2.5/weather/?q=[LOCATION]&appid=[ID]
+
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
 
 const data = {
   temperature: 5,
@@ -11,32 +18,51 @@ const data = {
   wind: "10 m/s"
 };
 
-const data2 = {
-  temperature: 15,
-  weatherState: WINDY,
-  humidity: 20,
-  wind: "10 m/s"
-};
-
 class WeatherLocation extends Component {
   constructor() {
     super();
     this.state = {
-      city: 'Bogotá',
+      city: "Bogotá",
       data: data
     };
   }
 
+  getWeatherState = weather_data => {
+    return SUN;
+  };
+
+  getData = weather_data => {
+    const { humidity, temp } = weather_data.main;
+    const { speed } = weather_data.wind;
+    const weatherState = this.getWeatherState(weather_data);
+
+    const data = {
+      humidity,
+      temperature: temp,
+      weatherState,
+      wind: `${speed} m/s`
+    };
+
+    return data;
+  };
+
   handleUpdateClick = () => {
-    console.log("actualizado");
-    this.setState({
-      city: 'Bogotá!',
-      data: data2
-    })
+    fetch(api_weather)
+      .then(resolve => {
+        return resolve.json();
+      })
+      .then(data => {
+        const newWeather = this.getData(data);
+        console.log(newWeather);
+        debugger
+        this.setState({
+          data: newWeather
+        });
+      });
   };
 
   render() {
-    const {city, data} = this.state
+    const { city, data } = this.state;
     return (
       <div className="weatherLocationCont">
         <Location city={city} />
