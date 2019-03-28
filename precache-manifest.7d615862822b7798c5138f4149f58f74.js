fa42e8f7651831a5fe9fa348e73c9e822c49b83e
Leonardo Omana <leotangram@gmail.com>
2019-03-27 22:12:26 -0500

    Updates

---
+++ precache-manifest.7d615862822b7798c5138f4149f58f74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72a6b7ef5541b624727a",
+    "url": "/react-weather-app/static/js/runtime~main.72a6b7ef.js"
+  },
+  {
+    "revision": "6c92ea8384c91bc1e014",
+    "url": "/react-weather-app/static/js/main.6c92ea83.chunk.js"
+  },
+  {
+    "revision": "e9627f4e02e98d7b9895",
+    "url": "/react-weather-app/static/js/1.e9627f4e.chunk.js"
+  },
+  {
+    "revision": "6c92ea8384c91bc1e014",
+    "url": "/react-weather-app/static/css/main.4b4fae01.chunk.css"
+  },
+  {
+    "revision": "e9627f4e02e98d7b9895",
+    "url": "/react-weather-app/static/css/1.50bbd5ae.chunk.css"
+  },
+  {
+    "revision": "81ed6a278c32e4bbdacff5334de779e0",
+    "url": "/react-weather-app/index.html"
+  }
+];
